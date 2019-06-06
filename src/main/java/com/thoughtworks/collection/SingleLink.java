278adabe17cd8f96fc471a0c44e48ee1b1b83715
LIShuning <lishiuning1955142623@outlook.com>
2019-06-06 15:10:19 +0800

    class SingleLink completed

--- src/main/java/com/thoughtworks/collection/SingleLink.java
+++ src/main/java/com/thoughtworks/collection/SingleLink.java
@@ -1,6 +1,40 @@
 package com.thoughtworks.collection;
 
-public interface SingleLink<T> {
-    public void addTailPointer(T item);         //添加尾指针
-    public T getNode(int index);
+import java.util.NoSuchElementException;
+
+public class SingleLink<T> {
+
+    private Node head = null;
+    private Node tail = null;
+
+    class Node {
+        private T item;
+        private Node next;
+        Node(T item) {
+            this.item = item;
+            this.next = null;
+        }
+    }
+
+    public void addTailPointer(T item) {
+        Node node = new Node(item);
+        if (this.tail != null) {
+            this.tail.next = node;
+            this.tail = this.tail.next;
+        } else {
+            this.tail = this.head = node;
+        }
+    }
+    public T getNode(int index) {
+        Node cur = this.head;
+        int postion = 1;
+        while (cur != null) {
+            if (postion == index) {
+                return cur.item;
+            }
+            cur = cur.next;
+            postion += 1;
+        }
+        throw new NoSuchElementException();
+    }
 }
